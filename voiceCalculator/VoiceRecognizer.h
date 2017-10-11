33375efc8ef1284d4c21df5b65a469e32a269feb
Serhii Kostiuk <k.seryoga@gmail.com>
2017-10-11 23:57:42 +0300

    add voiceRecornizer class

---
+++ voiceCalculator/VoiceRecognizer.h
@@ -0,0 +1,13 @@
+//
+//  VoiceRecognizer.h
+//  voiceCalculator
+//
+//  Created by Сергій Костюк on 10/10/17.
+//  Copyright © 2017 Сергій Костюк. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VoiceRecognizer : NSObject
+
+@end
---
+++ voiceCalculator/VoiceRecognizer.m
@@ -0,0 +1,13 @@
+//
+//  VoiceRecognizer.m
+//  voiceCalculator
+//
+//  Created by Сергій Костюк on 10/10/17.
+//  Copyright © 2017 Сергій Костюк. All rights reserved.
+//
+
+#import "VoiceRecognizer.h"
+
+@implementation VoiceRecognizer
+
+@end
